3d475ed20829d7605c40e68a4ada69ccc4db246c
Error44-Developer <75933735+Error44-Developer@users.noreply.github.com>
2020-12-13 12:55:54 +0100

    Create mute.py

---
+++ Sakura Mod/mute.py
@@ -0,0 +1,18 @@
+@bot.command()
+async def mute(ctx, member : discord.Member):
+    guild = ctx.guild
+    
+    for role in guild.roles:
+        if role.name == 'Muted':
+            await member.add_roles(role)
+            await ctx.send('{} has been muted'.format(member.mention))
+            return
+            
+            overwrite = discord.PermissionsOverwrite(send_messages=False)
+            newRole = await guild.create_role(name='Muted')
+            
+            for channel in guild.text_channels:
+                await guild.set_permissions(newRole,overwrite=overwrite)
+                
+            await member.add_roles(newRole)
+            await ctx.send('{} has been muted'.format(member.mention))
